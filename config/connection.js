d00e97fadfda962750ea7014786d9f8a95b65b0c
Effin <effinjoewilson@gmail.com>
2024-03-03 00:22:12 +0530

    Reestablished MongoDB connection setup

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,30 @@
-const mongoClient = require('mongodb').MongoClient;
-const state ={
-    db:null
+const { MongoClient } = require('mongodb');
+
+const state = {
+    db: null
 };
 
-
-module.exports.connect = function(done){
-    
+module.exports.connect = function(callback) {
     const url = "mongodb://localhost:27017";
-    const dbname='BuildLink'
-    mongoClient.connect(url, (err, data) => {
-        if (err) return done(err)
+    const dbname = 'BuildLink';
+    const client = new MongoClient(url);
 
-        
-        state.db=data.db(dbname)
-        
-    });
-
-    done();
+    client.connect()
+        .then(() => {
+            //console.log("Connected successfully to MongoDB");
+            state.db = client.db(dbname);
+            if (callback) {
+                callback(null); // Call the callback with no error
+            }
+        })
+        .catch(err => {
+            //console.error("Error connecting to MongoDB:", err);
+            if (callback) {
+                callback(err); // Call the callback with the error
+            }
+        });
 };
 
 module.exports.get = function() {
-    return state.db; 
+    return state.db;
 };
